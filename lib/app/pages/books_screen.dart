c6e42c8001f0c0b652c79cb1633923f6ff99a5ed
Melvax <melvin.adjei@gmail.com>
2023-10-08 12:51:00 +0200

    chapters list

--- lib/app/pages/books_screen.dart
+++ lib/app/pages/books_screen.dart
@@ -1,8 +1,13 @@
+import 'dart:developer';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:paourtest/controllers/book_controller.dart';
 import 'package:paourtest/controllers/providers/state_providers.dart';
+import 'package:paourtest/data/domain/books_model.dart';
+import 'package:paourtest/data/domain/chapter_model.dart';
+import 'package:paourtest/data/service/lesson_service.dart';
 
 class BookScreen extends ConsumerStatefulWidget {
   const BookScreen({super.key});
@@ -37,22 +42,12 @@ class _BookScreenState extends ConsumerState<BookScreen> {
               children: [
                 bookState.loading
                     ? const CircularProgressIndicator()
-                    : Column(
-                        children: bookState.books
+                    : Column(children: [
+                        ...bookState.books
                             .map(
                               (book) => Column(
                                 children: [
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      book.displayTitle ?? "nuull",
-                                      style: const TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.black,
-                                        fontSize: 24,
-                                      ),
-                                    ),
-                                  ),
+                                  Text(book.displayTitle ?? ""),
                                   Card(
                                     clipBehavior: Clip.antiAlias,
                                     shape: RoundedRectangleBorder(
@@ -64,9 +59,15 @@ class _BookScreenState extends ConsumerState<BookScreen> {
                                         InkWell(
                                           onTap: () {
                                             if (book.valid) {
-                                              print("valid");
-                                            } else {
-                                              print("invalid");
+                                              log("valid");
+                                              showDialog(
+                                                context: context,
+                                                builder: (context) => Padding(
+                                                  padding:
+                                                      const EdgeInsets.symmetric(horizontal: 32.0, vertical: 120.0),
+                                                  child: ChapterView(book: book),
+                                                ),
+                                              );
                                             }
                                           },
                                           child: CachedNetworkImage(
@@ -87,15 +88,104 @@ class _BookScreenState extends ConsumerState<BookScreen> {
                                       ],
                                     ),
                                   ),
-                                  SizedBox(height: 20),
+                                  const SizedBox(height: 20),
                                 ],
                               ),
                             )
                             .toList(),
-                      )
+                      ])
               ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class ChapterView extends StatefulWidget {
+  final BookModel book;
+
+  const ChapterView({
+    super.key,
+    required this.book,
+  });
+
+  @override
+  State<ChapterView> createState() => _ChapterViewState();
+}
+
+class _ChapterViewState extends State<ChapterView> {
+  LessonServices lessonServices = LessonServices();
+  List<ChapterModel> chapters = [];
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) async {
+        chapters = await lessonServices.fetchChapters(widget.book.id);
+        setState(() {
+          chapters = chapters;
+        });
+
+        // await ref.read(lessonController.notifier).fetchInitialBooks();
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Container(
+        height: 300,
+        width: 300,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          children: [
+            CachedNetworkImage(
+              imageUrl: widget.book.url ?? "",
+              height: 140,
+              width: double.infinity,
+              fit: BoxFit.cover,
+              errorWidget: (context, url, error) => Image.asset("assets/img/placeholder.png"),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              widget.book.displayTitle ?? "",
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              // height: 300,
+              child: ListView.builder(
+                itemCount: chapters.length,
+                itemBuilder: (context, index) => Material(
+                  child: ListTile(
+                    leading: SizedBox(
+                      width: 50,
+                      child: CachedNetworkImage(
+                        imageUrl: chapters[index].url,
+                        height: 140,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                        errorWidget: (context, url, error) => Image.asset("assets/img/placeholder.png"),
+                      ),
+                    ),
+                    title: Text(chapters[index].title),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
